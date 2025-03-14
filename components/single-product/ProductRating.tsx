7e82d9a61d946737c802f12873aae6038740026f
Cansu uğur <99813607+cansu05@users.noreply.github.com>
2025-03-14 13:26:15 +0300

    fix bug

--- components/single-product/ProductRating.tsx
+++ components/single-product/ProductRating.tsx
@@ -1,6 +1,7 @@
 import { FaStar } from "react-icons/fa6";
 
 const ProductRating = ({ productId }: { productId: string }) => {
+  console.log(productId);
   const rating = 4.2;
   const count = 25;
   const className = `flex gap-1 items-center text-md mt-1 mb-4`;
