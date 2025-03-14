36e741d31b2f5d6583bf9a62ea61f8197425b32b
Cansu uğur <99813607+cansu05@users.noreply.github.com>
2025-03-14 13:28:57 +0300

    fix

--- components/products/FavoriteToggleButton.tsx
+++ components/products/FavoriteToggleButton.tsx
@@ -2,6 +2,7 @@ import { IoHeart } from "react-icons/io5";
 import { Button } from "../ui/button";
 
 const FavoriteToggleButton = ({ productId }: { productId: string }) => {
+  console.log(productId);
   return (
     <Button size="icon" variant="outline" className="p-2 cursor-pointer">
       <IoHeart />
